5240727bd55888b1c9b24d0fd543cb114b8247da
JAEJIN <wowls9416@naver.com>
2023-12-28 17:54:03 +0900

    feat:[auth] jwt인증 작업 시작 전

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 컨트롤러에서 유저의 입력값을 자동으로 DTO 객체로 변환해줌
+    }),
+  );
+
+  await app.listen(3000);
+}
+
+bootstrap();
